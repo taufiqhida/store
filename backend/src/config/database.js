4b3be8801250bcc173701ed0d58070d2cf4a515d
taufiqhida <taufiqh2003@gmail.com>
2026-02-08 06:14:03 +0700

    ganti databasejs

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,11 @@
 const mariadb = require('mariadb');
 require('dotenv').config();
 
-const pool = mariadb.createPool(process.env.DATABASE_URL, {
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'taufiq_store_1',
     connectionLimit: 10
 });
 
