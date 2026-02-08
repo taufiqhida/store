47cd81d97d6169e0360a6555bb53ea8dc2f921c7
taufiqhida <taufiqh2003@gmail.com>
2026-02-08 20:23:56 +0700

    Refactor: Use separate env variables for database configuration

--- backend/prisma/seed-raw.js
+++ backend/prisma/seed-raw.js
@@ -11,7 +11,7 @@ async function main() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        port: 3306,
+        port: parseInt(process.env.DB_PORT) || 3306,
         multipleStatements: true
     });
 
